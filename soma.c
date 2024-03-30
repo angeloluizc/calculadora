31a7bc2ac019b33ed64ddc643eb83aebef2be742
angeloluizc <161398920+angeloluizc@users.noreply.github.com>
2024-03-29 21:14:49 -0300

    Criar o arquivo de soma
    Criando o arquivo em C para calcular a soma de dois números.

---
+++ soma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int a, b;
+  int resultado;
+  a = 2;
+  b = 3;
+  resultado = a + b;
+  printf ("O resultado da soma: %d\n", resultado);
+  return 0;
+}
